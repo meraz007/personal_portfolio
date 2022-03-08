8567f1d801512752bdb24418ae3928f3fe4c0b19
saiful Islam <saifulmeraz897@gmail.com>
2022-03-08 23:50:57 +0600

    create components

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Saiful Islam Miraj</h1>
+      <p>I'm <span>Front-End And Vue Js Developer</span></p>
+    </div>
+  </section><!-- End Hero -->
+</template>
